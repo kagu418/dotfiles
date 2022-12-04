4c4bb1ef2f261658f2cc2e060ba60dfe05fc1203
kagu418 <82737385+kagu418@users.noreply.github.com>
2022-12-04 09:13:06 +0900

    Add: install.sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+DOTFILES_DIR="$HOME/dotfiles"
+BREW_PREFIX="/opt/homebrew"
+REPO="https://github.com/kagu418/dotfiles"
+
+if [[ -d "$DOTFILES_DIR" ]]; then
+  printf "dotfiles already exists on this system.\n"
+  exit 1
+fi
+
+if ( hash git 2>/dev/null ); then
+  git clone --recursive "$REPO.git" "$DOTFILES_DIR"
+else
+  url="$REPO/archive/refs/heads/main.tar.gz"
+  output="$HOME/dotfiles.tar.gz"
+
+  if ( hash curl 2>/dev/null ); then
+    curl -L "$url" -o "$output"
+  elif ( hash wget 2>/dev/null ); then
+    wget "$url" -O "$output"
+  else
+    exit 1
+  fi
+    tar xzf "$output" && rm "$output" && mv "$HOME/dotfiles-main" "$DOTFILES_DIR"
+fi
+
+if [[ -d "$DOTFILES_DIR" ]]; then
+  printf "Succeeded to download repository to %s\n" "$DOTFILES_DIR"
+else
+  printf "Failed to download repository or extract tar.\n"
+  exit 1
+fi
+
+if ( hash brew 2>/dev/null ); then
+  printf "Homebrew already installed.\n"
+else
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+$BREW_PREFIX/bin/brew analytics off
+$BREW_PREFIX/bin/brew bundle -v --file="$DOTFILES_DIR/brew/.config/brew/Brewfile"
+
+for dir in "$DOTFILES_DIR"/*; do
+  [[ -d "$dir" ]] || continue
+  $BREW_PREFIX/bin/stow -d "$DOTFILES_DIR" -t "$HOME" -R -v "${dir#"$DOTFILES_DIR"/}"
+done
