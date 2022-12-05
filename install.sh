cce8f7384c6e799403589009823a8dd664936c47
kagu418 <82737385+kagu418@users.noreply.github.com>
2022-12-06 07:42:06 +0900

    feat: ask if you want to install Command Line Tools

--- install.sh
+++ install.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-DOTFILES_DIR="$HOME/dotfiles"
+DOTFILES_DIR="$HOME/.dotfiles"
 BREW_PREFIX="/opt/homebrew"
-REPO="https://github.com/kagu418/dotfiles"
+REPO="https://github.com/kagu418/dotfiles.git"
 
 if ! [[ -d "$(xcode-select -p)" ]]; then
-  printf "You must install Command Line Tools before running this shell script.\n" >&2
-  printf "Use \`xcode-select --install\`.\n" >&2
-  exit 1
+  printf "You must install Command Line Tools before running this shell script.\n"
+  read -rp "Do you want to install Command Line Tools now? [y/N] "
+  if [[ "${REPLY}" == [yY]* ]]; then
+    xcode-select --install
+    printf "Installing Command Line Tools...\n"
+    exit 1
+  else
+    printf "Use \`xcode-select --install\`.\n"
+    exit 1
+  fi
 fi
 
 if ( hash brew 2>/dev/null ); then
   printf "Skipping install Homebrew. It is already installed.\n"
 else
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-if ! [[ -x "$BREW_PREFIX/bin/brew" ]]; then
-  printf "Failed to install Homebrew.\n" >&2
-  exit 1
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 if [[ -d "$DOTFILES_DIR" ]]; then
-  printf "dotfiles already exists on this system.\n" >&2
+  printf "dotfiles already exists on \`%s\`.\n" "$DOTFILES_DIR" >&2
   exit 1
 fi
 
-git clone --recursive "$REPO.git" "$DOTFILES_DIR"
+git clone --recursive "$REPO" "$DOTFILES_DIR"
 
 if [[ -d "$DOTFILES_DIR" ]]; then
-  printf "Succeeded to clone repository to %s\n" "$DOTFILES_DIR"
+  printf "Succeeded to clone repository to \`%s\`.\n" "$DOTFILES_DIR"
 else
   printf "Failed to clone repository.\n" >&2
   exit 1
