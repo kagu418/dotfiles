80995e494856ecf18316f8d548050ced7c6fe2f1
kagu418 <82737385+kagu418@users.noreply.github.com>
2023-03-25 08:28:01 +0900

    update: refactor cmp.lua

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,4 +1,4 @@
-local M = {
+return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
@@ -8,49 +8,43 @@ local M = {
     "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind-nvim",
   },
-}
-
-local function config()
-  local cmp = require("cmp")
-
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-        require("luasnip").lsp_expand(args.body)
-      end,
-    },
-    mapping = cmp.mapping.preset.insert({
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete({}),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    }),
-    sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-      { name = "path" },
-      { name = "buffer", keyword_length = 4 },
-    }),
-    formatting = {
-      format = require("lspkind").cmp_format({
-        maxwidth = 50,
-        with_text = true,
-        menu = {
-          nvim_lsp = "[LSP]",
-          luasnip = "[snip]",
-          path = "[path]",
-          buffer = "[buf]",
-        },
+  opts = function()
+    local cmp = require("cmp")
+    return {
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete({}),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
-    },
-    experimental = {
-      native_menu = false,
-      ghost_text = true,
-    },
-  })
-end
-
-M.config = config
-
-return M
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "buffer", keyword_length = 4 },
+      }),
+      formatting = {
+        format = require("lspkind").cmp_format({
+          maxwidth = 50,
+          with_text = true,
+          menu = {
+            nvim_lsp = "[LSP]",
+            luasnip = "[snip]",
+            path = "[path]",
+            buffer = "[buf]",
+          },
+        }),
+      },
+      experimental = {
+        native_menu = false,
+        ghost_text = true,
+      },
+    }
+  end,
+}
